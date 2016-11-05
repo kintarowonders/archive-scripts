80fbbc8495b73b84ab21e14fffaf2ac5542ab10f
John Tate <john@johntate.org>
2016-11-06 09:41:39 +1100

    Added a variable to define how many days an archive is kept for.

---
+++ config.py
@@ -0,0 +1,22 @@
+# the path where archives are kept
+archivePath = "/archive/"
+
+# the paths which are excluded from backups to current
+excludePaths = []
+excludePaths.append("/sys")
+excludePaths.append("/proc")
+
+global archivePath
+global excludePaths
+
+# these are the systems for the script to back up.
+backupSystems = []
+backupSystems.append("system1.host.com")
+backupSystems.append("system2.host.com")
+backupSystems.append("system3.host.com")
+
+global backupSystems
+
+# how many days to archive for (integer)
+archiveForDays = 14
+global archiveForDays
