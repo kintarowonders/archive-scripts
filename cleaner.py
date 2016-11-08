f8028e27457b20f68493e657d3c372d353e18a0f
John Tate <john@johntate.org>
2016-11-08 06:45:59 +0000

    Did unit test of method getDestruDirList and got it working on production system

--- cleaner.py
+++ cleaner.py
@@ -3,23 +3,24 @@ import sys
 import datetime
 import config
 
-archivePath = config.archivePath
 global archivePath
-archiveForDays = config.archiveForDays
 global archiveForDays
+
+archivePath = config.archivePath
+archiveForDays = config.archiveForDays
 
 def getDestruDirList():
     # This method creates a list of files for deletion.
     
-    archiveList = os.listDir(archivePath) #get list of archive directorires
+    archiveList = os.listdir(archivePath) #get list of archive directorires
     deletionList = []                     #create list for deletion
 
     for dateStr in archiveList:
         skip = False
         if (not dateStr == "current"):
             dateVar = datetime.datetime.strptime(dateStr, "%Y-%m-%d")
-            if dateVar.date < datetime.datetime.now() - datetime.timedelta(days=archiveForDays):
+            if dateVar.date() < datetime.datetime.now().date() - datetime.timedelta(days=archiveForDays):
                 deletionList.append(archivePath + dateStr) #add full pathname to deletion list
-                print("Revision " + dateVar.strftime("%d-%m-%Y") + " to be deleted."
+                print("Revision " + dateVar.strftime("%d-%m-%Y") + " to be deleted.")
 
     return deletionList 
